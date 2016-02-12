c766fd493ffc9ac7bdb59bc0534547d4dcc78228
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-12 08:02:49 -0800

    Add getters and setters for Contact class.

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -7,5 +7,43 @@ namespace AdBook.Objects
     private string _name;
     private string _phoneNumber;
     private string _address;
+    private static List<Contact> _instances = new List<Contact> {};
+
+    public Contact (string name, string phoneNumber, string address)
+    {
+      _name = name;
+      _phoneNumber = phoneNumber;
+      _address = address;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _name = newPhoneNumber;
+    }
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+
+    public void SetAddress(string newAddress)
+    {
+      _name = newAddress;
+    }
   }
 }
