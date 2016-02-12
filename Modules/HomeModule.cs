077286ba60a1e46aa34dcc20bf0a83fa18bb29ae
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-12 09:16:11 -0800

    Debug code.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,28 +6,31 @@ namespace AddressBook
 {
   public class HomeModule : NancyModule
   {
-    Get["/contacts"] = _ => {
-      List<Contact> allContacts = Contact.GetAll();
-      return View["index.cshtml", allContacts];
-    };
+    public HomeModule()
+    {
+      Get["/contacts"] = _ => {
+        List<Contact> allContacts = Contact.GetAll();
+        return View["index.cshtml", allContacts];
+      };
 
-    Get["/contacts/new"] = _ => {
-      return View["add_new_contact"];
-    };
+      Get["/contacts/new"] = _ => {
+        return View["add_new_contact"];
+      };
 
-    Post["/contacts/added"] = _ => {
-      Contact newContact = new Contact (Resquest.Form["name"], (Resquest.Form["phoneNumber"], (Resquest.Form["address"]);
-      return View["contact_created.cshtml", newContact];
-    };
+      Post["/contacts/added"] = _ => {
+        Contact newContact = new Contact ((Request.Form["name"]), (Request.Form["phoneNumber"]), (Request.Form["address"]));
+        return View["contact_created.cshtml", newContact];
+      };
 
-    Get["contacts/{id}"] = parameters => {
-      Contact selectedContact = Contact.Find(parameters.id);
-      return View["contact_details.cshtml", selectedContact]
+      Get["contacts/{id}"] = parameters => {
+        Contact selectedContact = Contact.Find(parameters.id);
+        return View["contact_details.cshtml", selectedContact];
+      };
+
+      Post["/contacts/deleted"] = _ => {
+        Contact.ClearAll();
+        return View["contacts_deleted.cshtml"];
+      };
     }
-
-    Post["/contacts/deleted"] = _ => {
-      Contact.ClearAll();
-      return View["contacts_deleted.cshtml"];
-    };
   }
 }
