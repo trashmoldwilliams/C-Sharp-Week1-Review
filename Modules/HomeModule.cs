1bce15652e3a24e341cedc3ba65c392663c37d7a
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-12 08:28:51 -0800

    Add path for new contact form.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,12 +9,19 @@ namespace AddressBook
     Get["/"] = _ => {
       List<Contact> allContacts = Contact.GetAll();
       return View["index.cshtml", allContacts];
-    }
+    };
+
+    Get["/add_new_contact"] = _ => View["add_new_contact"];
 
     Post["/contact_created"] = _ => {
       Contact newContact = new Contact (Resquest.Form["name"], (Resquest.Form["phoneNumber"], (Resquest.Form["address"]);
       newContact.Save();
       return View["contact_created.cshtml", newContact];
+    };
+
+    Post["/contacts_deleted"] = _ => {
+      Contact.ClearAll();
+      return View["contacts_deleted"];
     };
 
   }
