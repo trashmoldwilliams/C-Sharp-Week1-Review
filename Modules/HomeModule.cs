04c532d7aa44f91ba6c607dd76fa34438cc22e28
Will Johnson <trashmoldwilliams@gmail.com>
2016-02-12 08:25:39 -0800

    Add pathing for home and contact creation pages.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,6 +6,16 @@ namespace AddressBook
 {
   public class HomeModule : NancyModule
   {
-    Get["/"] = _ => View[""];
+    Get["/"] = _ => {
+      List<Contact> allContacts = Contact.GetAll();
+      return View["index.cshtml", allContacts];
+    }
+
+    Post["/contact_created"] = _ => {
+      Contact newContact = new Contact (Resquest.Form["name"], (Resquest.Form["phoneNumber"], (Resquest.Form["address"]);
+      newContact.Save();
+      return View["contact_created.cshtml", newContact];
+    };
+
   }
 }
